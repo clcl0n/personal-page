1852cc97d44fe81f8ca23760ac7cae0fbdc3704e
clcl0n <patrik.f.balaz@outlook.com>
2021-06-13 07:12:34 +0200

    🔥 eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import GameOfLifePage from './components/game-of-life-page';
 import Home from './components/home';
 
-
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Route exact path="/game_of_life">
-          <GameOfLifePage />
-        </Route>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Route exact path="/game_of_life">
+                    <GameOfLifePage />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLink } from '@fortawesome/free-solid-svg-icons/faLink';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt';
 import App from './App';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLink } from '@fortawesome/free-solid-svg-icons/faLink'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt'
 
 library.add(faLink, faEnvelope, faMapMarkerAlt);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
