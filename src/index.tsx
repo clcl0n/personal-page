92e49ecc91289d11b313aab41627027820e49ff7
clcl0n <patrik.f.balaz@outlook.com>
2021-06-13 06:59:10 +0200

    github like main page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLink } from '@fortawesome/free-solid-svg-icons/faLink'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt'
+
+library.add(faLink, faEnvelope, faMapMarkerAlt);
 
 ReactDOM.render(
   <React.StrictMode>
